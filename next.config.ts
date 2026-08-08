5382a3612b768dac074b89cc35028460162642cf
sanjay s <160470363+Sanjay9309SureshKumar@users.noreply.github.com>
2026-08-08 22:39:49 +0530

    feat: implement about-us page and add route redirect from /about

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/about-us",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
